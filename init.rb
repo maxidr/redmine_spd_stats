ac04f93475429cc5abb3de64ee5d8590b6d6adf6
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-04-10 15:44:00 -0300

    Generación del plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_spd_stats do
+  name 'Redmine SPD stats plugin'
+  author 'Maximiliano Dello Russo'
+  description 'Este plugin de redmine permite visualizar una serie de datos estadísticos de la plataforma SPD'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'https://coderwall.com/maxidr'
+end
