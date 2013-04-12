e2292a3e2fa7836bc7d7efb1d3d9ca7a2695b28b
Maximiliano Dello Russo <mdellorusso@aonken.com.ar>
2013-04-12 14:16:01 -0300

    Se incrementa la versión y el README

--- init.rb
+++ init.rb
@@ -6,8 +6,8 @@ Redmine::Plugin.register :redmine_spd_stats do
   name 'Redmine SPD stats plugin'
   author 'Maximiliano Dello Russo'
   description 'Este plugin de redmine permite visualizar una serie de datos estadísticos de la plataforma SPD'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  version '0.1.0'
+  url 'https://github.com/maxidr/redmine_spd_stats'
   author_url 'https://coderwall.com/maxidr'
 
   #Redmine::MenuManager.map :top_menu do | 
